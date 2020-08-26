02da1e485ff1d7b94aab8699c91dfac752f3175d
davenportnw <davenportnw@gmail.com>
2020-08-26 12:58:06 -0500

    Add routes.

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express()
 const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+const db = require('./models');
+
 
 const PORT = 8080;
+
 
 const routes = require('./routes/routes');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+});
+
 app.use(express.static('public'));
 app.use(routes);
-
 
 
 
